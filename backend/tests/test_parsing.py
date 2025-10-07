6d9c7af98b759c1f754a76b89e0fd0c01d1ca475
Luke-Kissling <luke.kissling@gmail.com>
2025-10-07 16:40:14 -0400

    this time for real

--- backend/tests/test_parsing.py
+++ backend/tests/test_parsing.py
@@ -1,7 +1,7 @@
 """Tests for item parsing functionality."""
 
 import pytest
-from core.parsing import ItemParser, ParsedItem
+from backend.core.parsing import ItemParser, ParsedItem
 
 
 class TestItemParser:
